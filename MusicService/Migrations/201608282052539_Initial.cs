0bbccdcdbc02aa0072a20a1dabb4adaa7a1341e7
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-28 17:58:37 -0300

    Created the database again

---
+++ MusicService/Migrations/201608282052539_Initial.cs
@@ -0,0 +1,78 @@
+namespace MusicService.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Albums",
+                c => new
+                    {
+                        AlbumId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        ReleaseYear = c.Int(nullable: false),
+                        BandRefId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.AlbumId)
+                .ForeignKey("dbo.Bands", t => t.BandRefId, cascadeDelete: true)
+                .Index(t => t.BandRefId);
+            
+            CreateTable(
+                "dbo.Bands",
+                c => new
+                    {
+                        BandId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Genre = c.String(),
+                        Country = c.String(),
+                        LabelRefId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.BandId)
+                .ForeignKey("dbo.Labels", t => t.LabelRefId, cascadeDelete: true)
+                .Index(t => t.LabelRefId);
+            
+            CreateTable(
+                "dbo.Labels",
+                c => new
+                    {
+                        LabelId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        FoundationYear = c.Int(nullable: false),
+                        FounderName = c.String(),
+                    })
+                .PrimaryKey(t => t.LabelId);
+            
+            CreateTable(
+                "dbo.Tracks",
+                c => new
+                    {
+                        TrackId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Length = c.String(),
+                        Number = c.Int(nullable: false),
+                        AlbumRefId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.TrackId)
+                .ForeignKey("dbo.Albums", t => t.AlbumRefId, cascadeDelete: true)
+                .Index(t => t.AlbumRefId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Tracks", "AlbumRefId", "dbo.Albums");
+            DropForeignKey("dbo.Albums", "BandRefId", "dbo.Bands");
+            DropForeignKey("dbo.Bands", "LabelRefId", "dbo.Labels");
+            DropIndex("dbo.Tracks", new[] { "AlbumRefId" });
+            DropIndex("dbo.Bands", new[] { "LabelRefId" });
+            DropIndex("dbo.Albums", new[] { "BandRefId" });
+            DropTable("dbo.Tracks");
+            DropTable("dbo.Labels");
+            DropTable("dbo.Bands");
+            DropTable("dbo.Albums");
+        }
+    }
+}
